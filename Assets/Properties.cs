ed16f2c75d2cb156bb0e03fda5e8ad0e85af7b12
Sharon Wanjiru <sharon.macharia@admi.ac.ke>
2019-05-17 12:52:12 +0300

    first commit

---
+++ Assets/Properties.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Properties : MonoBehaviour
+{
+
+    public  float mymass = 10f;
+    public float mydrag = 1.2f;
+    public  float myforce = 10f;
+    public float mymass2 = 10f;
+    public GameObject mygameobject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Renderer rb = mygameobject.GetComponent<Renderer>();
+        /*rb.mass = mymass;
+        rb.drag = mydrag;
+        rb.AddForce(Vector3.up * myforce);
+        */
+        rb.material.color = Color.blue;
+        mybehaviour();
+    }
+    void mybehaviour()
+    {
+        Rigidbody rb2 = GetComponent<Rigidbody>();
+        rb2.mass = mymass2;
+    }
+    // finding child game objects
+
+}
---
+++ Assets/Properties2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Properties2 : MonoBehaviour
+{
+    //array variable
+    public Transform[] waypoints;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoints = new Transform[transform.childCount];
+        int i=20;
+        foreach(Transform t in transform)
+        {
+            waypoints[i++] = t;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //  Finding Gameobjects by name or tag
+    void OnTriggerEnter(GameObject other)
+    {
+        other.tag = "Cube";
+    }
+}
